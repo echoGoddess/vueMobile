e4f6512047061cc14585c980db1e601ad34dc2bf
yuanyuan.wang <yuanyuan.wang@chinaredstar.com>
2018-03-28 17:55:07 +0800

    修改主页和登陆页

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,27 +1,35 @@
 <template>
   <div class="home">
+    <mt-header fixed title="我是主页">
+      <router-link to="/" slot="left">
+        <mt-button icon="back">返回</mt-button>
+      </router-link>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
     <h1>主页</h1>
-    <router-link to="/Login">toLogin</router-link>
+    <router-link class="toLoginPage" to="/Login">toLoginPage</router-link>
   </div>
 </template>
 <script>
-  export default {
-    data:{
-      content:'主页'
-    },
-    methods:{}
-  }
+import { Header } from "mint-ui";
+
+export default {
+  data: {
+    content: "主页"
+  },
+  methods: {}
+};
 </script>
 <style scoped>
-  div {
-    font-size: 40px;
-  }
-  a{
-    border:2px solid #ddd;
-    line-height:24px;
-    color:tomato;
-    background: #fff;
-    border-radius: 14px;
-    text-decoration: none;
-  }
+div {
+  font-size: 40px;
+}
+.toLoginPage {
+  border: 2px solid #ddd;
+  line-height: 24px;
+  color: tomato;
+  background: #fff;
+  border-radius: 14px;
+  text-decoration: none;
+}
 </style>
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,33 +1,45 @@
 <template>
   <div class="login">
+    <mt-header fixed title="我是登陆页">
+      <router-link to="/" slot="left">
+        <mt-button icon="back">返回</mt-button>
+      </router-link>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
     <h1>{{content}}</h1>
-    <button  v-on:click="toHome">toHome</button>
+    <form action="">
+      <mt-field label="用户名" placeholder="请输入用户名" v-model="username"></mt-field>
+      <mt-field label="密码" placeholder="请输入密码" type="password" v-model="password"></mt-field>
+      <mt-button type="primary" size="large" @click.native="toHome">登陆</mt-button>
+    </form>
   </div>
 </template>
 <script>
-  export default {
-    name: 'login',
-    data() {
-      return {
-        content: '登陆页'
-      }
-    },
-    methods: {
-      toHome() {
-        this.$router.push({name: 'Home'});
-      }
+import { Header } from "mint-ui";
+import { Field } from "mint-ui";
+import { Button } from 'mint-ui';
+
+export default {
+  name: "login",
+  data() {
+    return {
+      content: "登陆页",
+      username:'',
+      password:'',
+    };
+  },
+  methods: {
+    toHome() {
+      this.$router.push({ name: "Home" });
     }
-  };
+  }
+};
 </script>
 <style scoped>
-  div {
-    font-size: 40px;
-  }
-  button{
-    border:2px solid #ddd;
-    line-height:24px;
-    color:tomato;
-    background: #fff;
-    border-radius: 14px;
-  }
+div {
+  font-size: 30px;
+}
+button{
+  margin-top:20px;
+}
 </style>
