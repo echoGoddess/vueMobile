f0a84d844c160990ff974d2a8e2e118ea57e1fc3
yuanyuan.wang <yuanyuan.wang@chinaredstar.com>
2018-03-28 17:16:54 +0800

    构建框架

---
+++ src/pages/home/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <h1>主页</h1>
+    <router-link to="/Login">toLogin</router-link>
+  </div>
+</template>
+<script>
+  export default {
+    data:{
+      content:'主页'
+    },
+    methods:{}
+  }
+</script>
+<style scoped>
+  div {
+    font-size: 40px;
+  }
+  a{
+    border:2px solid #ddd;
+    line-height:24px;
+    color:tomato;
+    background: #fff;
+    border-radius: 14px;
+    text-decoration: none;
+  }
+</style>
---
+++ src/pages/login/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="login">
+    <h1>{{content}}</h1>
+    <button  v-on:click="toHome">toHome</button>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'login',
+    data() {
+      return {
+        content: '登陆页'
+      }
+    },
+    methods: {
+      toHome() {
+        this.$router.push({name: 'Home'});
+      }
+    }
+  };
+</script>
+<style scoped>
+  div {
+    font-size: 40px;
+  }
+  button{
+    border:2px solid #ddd;
+    line-height:24px;
+    color:tomato;
+    background: #fff;
+    border-radius: 14px;
+  }
+</style>
